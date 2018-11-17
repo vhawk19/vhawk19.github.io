1180ea06d4fd92ad5305a73acd4632959cefff9d
vhawk19 <varunhawk19@gmail.com>
2018-11-17 23:43:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,3 +108,21 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
+
+  var options = {
+    strings: [
+      "I like to code",
+      "I like to write",
+      "I like to play",
+      "I like to listen"
+    ],
+    typeSpeed: 50,
+    startDelay: 500,
+    backSpeed: 35,
+    backDelay: 500,
+    loop: true,
+    loopCount: Infinity
+  };
+
+  var typed = new Typed(".subtitle", options);
+}
